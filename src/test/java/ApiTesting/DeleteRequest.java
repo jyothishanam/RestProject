c2c32d9a5937842c5c2c2a0fc2b166919361403c
jyothishanam <jyothi.shanam@gmail.com>
2019-06-22 12:55:17 +0100

    updated

--- src/test/java/ApiTesting/DeleteRequest.java
+++ src/test/java/ApiTesting/DeleteRequest.java
@@ -10,7 +10,7 @@ public class DeleteRequest {
 	
 @Test
 public void DelReq() {
-	//trying to commit from jyothi branch
+	//trying to commit from master branch
 	
 	Response response=RestAssured.delete("https://petstore.swagger.io/v2/pet/130");
 	
